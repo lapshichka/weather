25384718eefa3f65d5be5896180bb43b102a5c80
katen <katena.lapshina.03@mail.ru>
2023-09-14 13:25:37 +0300

    add dark theme in dark-mode

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 import "./style.css";
+
+
+
+// const apiKey = "bd08b02831e4494fb7a144059231209";
+
+// //http://api.weatherapi.com/v1/current.json?key=bd08b02831e4494fb7a144059231209&q=London
+
+// const query = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=London`;
