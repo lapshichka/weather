171ac047c1a47aa2a06aa513f653dfec0bde4057
katen <katena.lapshina.03@mail.ru>
2023-09-15 21:03:54 +0300

    fix dark theme in main.js

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
-const link = document.querySelector("#style");
-const dark = "/style/dark-mode.css";
-const light = "/style/style.css";
+const lightStylesheet = document.getElementById("light");
+const darkStylesheet = document.getElementById("dark");
+
+darkStylesheet.disabled = true;
 
 const slider = document.querySelector(".slider");
 let heading = document.querySelector(".switch-text");
 
 slider.addEventListener("click", () => {
-  link.href = link.href.includes(light)
-    ? window.location.origin + dark
-    : window.location.origin + light;
+  if (darkStylesheet.disabled) {
+    darkStylesheet.disabled = false;
+    lightStylesheet.disabled = true;
+    heading.textContent = "Dark mode";
+    return;
+  }
+  darkStylesheet.disabled = true;
+  lightStylesheet.disabled = false;
+  heading.textContent = "Light mode";
 });
-
-// const apiKey = "bd08b02831e4494fb7a144059231209";
-
-// //http://api.weatherapi.com/v1/current.json?key=bd08b02831e4494fb7a144059231209&q=London
 
 // const query = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=London`;
